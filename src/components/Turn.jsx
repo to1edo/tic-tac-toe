b0c4f1a13c448d739dc81bd3e70aebf313495a3f
carlos toledo <to1edo>
2023-01-11 04:02:58 -0400

    fix turn

--- src/components/Turn.jsx
+++ src/components/Turn.jsx
@@ -9,7 +9,7 @@ const Turn = ({marca}) => {
         <p className="text-[#ec1652] font-black text-4xl">X</p>
       </div>
       <div className={`${marca === 'O' ? 'border-2 border-white':''} text-center px-4 py-1 rounded-xl bg-[#101032]`}>
-        <p className="text-white font-bold">One</p>
+        <p className="text-white font-bold">Two</p>
         <p className="text-[#ffcc39] font-black text-4xl">X</p>
       </div>
 
